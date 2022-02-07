0a42d24ef4506de4a62ddd7ce0824819ac4bba08
Dante-Cand <cand.dante@gmail.com>
2022-02-07 16:27:36 +0100

    pre merge

---
+++ Dante-Bataille-Corse/Deck.js
@@ -0,0 +1,34 @@
+const suits = ["♠", "♣", "♥", "♦"];
+const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
+export default class Deck {
+    constructor(cards = newDeck()) {
+        this.cards = cards
+    }
+}
+numberOfCards(); {
+    return this.cards.length;
+}
+
+shuffle(); {
+    for (let i = this.numberOfCards -1; i>0; i--){
+        const newIndex = Math.floor(Math.random() * (i+1))
+        const oldValue = this.cards[newIndex]
+        this.cards[newIndex] = this.cards[i]
+        this.cards[i] = oldValue
+    }
+}
+
+class Card {
+    constructor(suit, value){
+        this.suit = suit
+        this.value = value
+    }
+}
+function newDeck() {
+    return suits.flatMap(suit => {
+        return values.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
