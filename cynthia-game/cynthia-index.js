54bd61bfbf9376a0f42e1011bcc6c813d9ea47e8
CDDC <cynthia.decelle@gmail.com>
2022-02-07 16:37:44 +0100

    Create todo's in JS

---
+++ cynthia-game/cynthia-index.js
@@ -0,0 +1,16 @@
+//THIS IS A MEMORY GAME WITH PAIRS OF IMAGES
+
+//TODO: create array of imgages with character name X2
+//TODO: create shuffle for images
+//TODO: display back of shuffled images
+//TODO: create eventlistener for each image on click
+//TODO: create conditions of the game:
+        // - if two cards are selected no more clicks possible (add message to player)
+        // - compare images:
+            // - if the two img are the same they stay visible and two clicks are possible again (add message to player)
+            // - if the two img are not the same the img dissapears after a few seconds and clicks are possible again (add message to player)
+            // - if all the img are visible the game is done (add message to player)
+            // - play again button to start new session
+
+//TODO: create play again button
+//TODO: create event listener for button on click
