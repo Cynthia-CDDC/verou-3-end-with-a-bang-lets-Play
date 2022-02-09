7dad8c49ef9bfd74aa050ecee9a1148e24086097
Feruzteame <teamefitwi@gmail.com>
2022-02-09 12:14:51 +0100

    add some css

--- Quiz-Game/script/fail.js
+++
@@ -1,5 +0,0 @@
-export function fail(){
-    const sound3 = document.getElementById("sound3")
-    console.log("fail")
-    sound3.play()
-  }
--- Quiz-Game/src/score.js
+++ Quiz-Game/src/score.js
@@ -2,6 +2,7 @@ import { score } from "./question.js";
 
 export function getSum(){
     const scoreHolder = document.getElementById("scoreHolder")
+    scoreHolder.id = "scoreHolder"
     const sum = score.reduce((a, b) => a + b, 0);
     scoreHolder.innerHTML = sum
 }
