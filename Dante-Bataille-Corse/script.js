01883f892085aea503d90d919cdf6f72ab672971
Dante-Cand <cand.dante@gmail.com>
2022-02-11 10:53:37 +0100

    wipe

--- Dante-Bataille-Corse/script.js
+++ Dante-Bataille-Corse/script.js
@@ -155,10 +155,20 @@ todo: same card tap rule
 todo: normal win mode 
 todo: new round button*/
 
+const suits = ["♠", "♣", "♥", "♦"];
+const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 
+cards = []
 
+for(suit in suits) {
+    for(value in values) {
+        cards.append(Card(suit, value))
+    }
+}
 
+var deck = Deck(cards)
 
+deck
 
 
 
