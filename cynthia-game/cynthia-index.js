0d643ff4a6b315f915489c162c01a216e1b20207
CDDC <cynthia.decelle@gmail.com>
2022-02-12 21:25:51 +0100

    JS Create reload-button

--- cynthia-game/cynthia-index.js
+++ cynthia-game/cynthia-index.js
@@ -44,69 +44,82 @@ const images = [
     },
 ];
 
+//imagesShuffled is now the images array shuffled.
 const imagesShuffled = images.sort((a, b) => 0.5 - Math.random());
-console.log(imagesShuffled)
 
 const clickedItems = [];
+const equalPick = [];
 
-for (let i = 0; i < images.length; i++) {
-    const card = document.createElement("div"); //create at least one element to use template literal.
-    card.className = "card";
-    card.id = imagesShuffled[i].character;
-    card.innerHTML = `
-        <div class="inner-card" id="inner-card">
-            <div class="card-front">
-                <img class="front-img" src="./cynthia-images/cheshire-cat.jpg" alt="Cheshire Cat">
-            </div>
-            <div class="card-back">
-                <img class="card-head" src= ${imagesShuffled[i].img}>
-                <h3 class="card-body"> ${imagesShuffled[i].character}</h3>
-            </div>
-        </div>`;
-    // Add index attribute
-    card.setAttribute("index", i);
-    const container = document.querySelector(".container");
-    container.appendChild(card);
-    
-    function imageClicked(event) {
-        const clickedCard = event.currentTarget;
-        console.log('----')
-        console.log(event.target)
-        console.log(event.currentTarget)
-        clickedCard.classList.toggle("flipCard");
-        clickedCard.children[0].classList.toggle("inner-card");//change the class of the cards childNode to trigger other css
-
-        clickedItems.push(clickedCard)//after toggles for classList to be correct
-        console.log(clickedItems) //works for item not for index (undifined)
-
-        if (clickedItems.length === 2) {
-            conditions ()
-        }
-        function conditions () {
-            if (clickedItems[0].outerText !== clickedItems[1].outerText) {
-                //not the same
-                console.log('chicken')
-                
-                const myTimeout = setTimeout(() => {
-                    for (let clickedItem of clickedItems) {
-                        clickedItem.classList.toggle("flipCard");
-                        clickedItem.children[0].classList.toggle("inner-card");
-                        clickedItem.addEventListener('click', imageClicked, { once: true });
+    for (let i = 0; i < imagesShuffled.length; i++) {
+        const card = document.createElement("div"); //create at least one element to use template literal.
+        card.className = "card";
+        card.id = imagesShuffled[i].character;
+        card.innerHTML = `
+            <div class="inner-card" id="inner-card">
+                <div class="card-front">
+                    <img class="front-img" src="./cynthia-images/cheshire-cat.jpg" alt="Cheshire Cat">
+                </div>
+                <div class="card-back">
+                    <img class="card-head" src= ${imagesShuffled[i].img}>
+                    <h3 class="card-body"> ${imagesShuffled[i].character}</h3>
+                </div>
+            </div>`;
+        // Add index attribute
+        card.setAttribute("index", i);
+        const container = document.querySelector(".container");
+        container.appendChild(card);
+        
+        function imageClicked(event) {
+            const clickedCard = event.currentTarget;
+            clickedCard.classList.toggle("flipCard");
+            clickedCard.children[0].classList.toggle("inner-card");//change the class of the cards childNode to trigger other css
+            
+            clickedItems.push(clickedCard)//after toggles for classList to be correct
+            console.log(clickedItems) //works for item not for index (undifined)
+            
+            if (clickedItems.length === 2) {
+                conditions ()
+            }
+            function conditions () {
+                if (clickedItems[0].outerText !== clickedItems[1].outerText) {
+                    //not the same
+                    const myTimeout = setTimeout(() => {
+                        for (let clickedItem of clickedItems) {
+                            clickedItem.classList.toggle("flipCard");
+                            clickedItem.children[0].classList.toggle("inner-card");
+                            clickedItem.addEventListener('click', imageClicked, { once: true });
+                        }
+                        emptyArray () // ok   
+                    }, 1000);
+                    
+                } else if(clickedItems[0].outerText === clickedItems[1].outerText) {
+                    //the same
+                    //cards stay visible: ok.
+                    equalPick.push(clickedCard)
+                    fiveEqualPicks ()
+                    emptyArray ()// ok 
+                } 
+                function emptyArray () {
+                    clickedItems.length = 0;
+                    console.log(clickedItems)
+                }
+                function fiveEqualPicks () {
+                    if (equalPick.length === 5) {
+                        const button = document.createElement('button');
+                        button.type = 'button';
+                        button.className = 'playAgain-button';
+                        button.innerHTML = 'Play Again';
+                        const presentation = document.querySelector('.presentation');
+                        presentation.appendChild(button);
+                        function startGame() {
+                            location.reload();
+                        }
+                    button.addEventListener('click', startGame)
                     }
-                    emptyArray () // ok   
-                }, 1000);
-                
-            } else if(clickedItems[0].outerText === clickedItems[1].outerText) {
-                //the same
-                //cards stay visible: ok.
-                console.log('horse')
-                emptyArray ()// ok
-            }
-            function emptyArray () {
-                clickedItems.length = 0;
-                console.log(clickedItems)
+                }
             }
         }
-    }
-card.addEventListener('click', imageClicked, { once: true })
+    card.addEventListener('click', imageClicked, { once: true })
 }
+
+
